09559fab9fd84efd744be4aecc18b32a52e46bf8
Björn Nilsson <djupblue@gmail.com>
2013-02-18 00:20:02 +0100

    Removed user settings from core

--- core/prelude-ui.el
+++ core/prelude-ui.el
@@ -74,16 +74,6 @@
       scroll-conservatively 100000
       scroll-preserve-screen-position 1)
 
-;; mode line settings
-(line-number-mode t)
-(column-number-mode t)
-(size-indication-mode t)
-
-;; make the fringe (gutter) smaller
-;; the argument is a width in pixels (the default is 8)
-(if (fboundp 'fringe-mode)
-    (fringe-mode 4))
-
 ;; enable y/n answers
 (fset 'yes-or-no-p 'y-or-n-p)
 
@@ -93,9 +83,6 @@
       '("" invocation-name " Prelude - " (:eval (if (buffer-file-name)
                                             (abbreviate-file-name (buffer-file-name))
                                           "%b"))))
-
-;; use zenburn as the default theme
-(load-theme 'zenburn t)
 
 (provide 'prelude-ui)
 ;;; prelude-ui.el ends here
